a813a21cf82bd59b59a1a9ae7ebaeaaa97debd84
rahathasanDev <devrahat.hasan@gmail.com>
2024-11-21 21:16:01 +0600

    Payload and TypeSafe redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/features/counterSlice";
+import { decrement, increment, incrementByValue } from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
   // const count = useSelector((state) => state.counter.count);
-  const {count} = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+  const {count} = useAppSelector((state ) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <>
       <div className="h-screen w-full">
@@ -13,6 +13,7 @@ function App() {
         </h1>
 
         <div className="flex justify-center items-center m-10">
+          <button onClick={()=> dispatch(incrementByValue(5))} className="px-3 mr-2 py-2 rounded-md text-2xl bg-yellow-600 text-white   font-semibold">IncrementByValue</button>
           <button onClick={()=> dispatch(increment())} className="px-3 py-2 rounded-md text-2xl bg-green-600 text-white   font-semibold">Increment</button>
           <h1 className="text-2xl px-2">{count}</h1>
           <button onClick={()=> dispatch(decrement())} className="px-3 py-2 rounded-md text-2xl bg-red-500 text-white   font-semibold">Decrement</button>
